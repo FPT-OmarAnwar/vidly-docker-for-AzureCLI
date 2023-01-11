ccb5f81a0ffbace70f21f66569e6e7deb931f58b
Omar Anwar <omar.anwar@finplus.tech>
2023-01-11 17:13:31 +0500

    second commit

---
+++ test_down.sh
@@ -0,0 +1,17 @@
+#!/bin/bash          
+
+STR="Hello World!"
+export STR="Hello World! from EXPORT"
+
+echo "Enter frontend image name"
+
+read frontend_image
+
+export frontend_image
+export frontend_image_path="./frontend"
+export backend_image="backend"
+export backend_image_path="./backend"
+export db_image="mongo:4.0-xenial"
+
+
+docker-compose down
